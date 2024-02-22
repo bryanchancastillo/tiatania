ef65065697bda927c561f54f3be2dccb786b6196
bryanchancastillo <bryanchancastillo15@gmail.com>
2024-02-22 16:01:24 -0500

    #13

---
+++ tiatania.DAL/Migrations/20240222204231_AddRoles.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tiatania.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "security_role",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Name", "NormalizedName" },
+                values: new object[] { "System User", "SYSTEM USER" });
+
+            migrationBuilder.InsertData(
+                table: "security_role",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { 2, null, "System Admin", "SYSTEM ADMIN" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "security_role",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "security_role",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Name", "NormalizedName" },
+                values: new object[] { "System Admin", "SYSTEM ADMIN" });
+        }
+    }
+}
