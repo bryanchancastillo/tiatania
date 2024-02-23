4f4fefcffef4e743f9ff51cc605f1733b5cb44b0
bryanchancastillo <bryanchancastillo15@gmail.com>
2024-02-23 00:53:02 -0500

    13

--- tiatania.API/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ tiatania.API/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -125,6 +125,24 @@ namespace tiatania.API.Areas.Identity.Pages.Account
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    user.CreatedBy = user.Id;
+                    user.UpdatedBy = user.Id;   
+
+                    var userRole = new UserRole
+                    {
+                        UserId = user.Id,
+                        RoleId = 1,
+                        Active = true,
+                        CreatedBy = user.Id,
+                        CreatedOn = DateTime.UtcNow,
+                        UpdatedBy = user.Id,
+                        UpdatedOn = DateTime.UtcNow
+                    };
+
+                    user.Roles.Add(userRole);
+                    await _userManager.UpdateAsync(user);
+
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
